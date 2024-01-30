556bdcc76190bd1da11f039648d916eb26ae2277
HaltingColt522 <l.frotschi@web.de>
2024-01-30 20:36:02 +0100

    first question

---
+++ unit1.pas
@@ -0,0 +1,64 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    CheckBox4: TCheckBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  If CheckBox1.checked then
+     label4.Show
+     label5.close
+  else
+     label5.show
+     label4.close
+     ;
+
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+
+end;
+
+
+
+end.
+
